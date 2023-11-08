b35e542aecfec9d9456653881d061e369fce7d47
Кристина <dubininivan1990@gmail.com>
2023-11-08 10:43:45 +0400

    Выполнено домашнее задание. Дописана логика

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,40 +1,96 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     static String booksInfo = "";
-
+    static int count;
 
     public static void main(String[] args) {
+
         while (true) {
+
             String name = inputBookName();
             int pageCount = inputPageCount();
-            addBook(name);
-            addBook(name, pageCount);
-            //todo использовать новый метод тут
+            int priceBook = inputPrice();
+
+            if (priceBook == 0 && pageCount == 0) {
+                addBook(name);
+            } else if (priceBook == 0) {
+                addBook(name, pageCount);
+            } else {
+                addBook(name, pageCount, priceBook);
+            }
             printInfo();
+
+            int check = checkContinue();
+
+            if (check == 2) {
+                break;
+            }
         }
     }
 
     public static String inputBookName() {
+        count++;
         System.out.println("Введите название книги:");
-        return new Scanner(System.in).nextLine();
+        return count + ". " + new Scanner(System.in).nextLine();
     }
 
     public static int inputPageCount() {
-        System.out.println("Введите кол-во страниц:");
-        return new Scanner(System.in).nextInt();
+        while (true) {
+            try {
+                System.out.println("Введите кол-во страниц:");
+                return new Scanner(System.in).nextInt();
+            } catch (InputMismatchException x) {
+                System.out.println("Вы ввели не число.");
+            }
+        }
+    }
+
+    public static int inputPrice() {
+        while (true) {
+            try {
+                System.out.println("Введите цену книги в рублях:");
+                return new Scanner(System.in).nextInt();
+            } catch (InputMismatchException x) {
+                System.out.println("Вы ввели не число.");
+            }
+        }
     }
 
     public static void addBook(String bookName) {
-        addBook(bookName, 0);
+        addBook(bookName, 0, 0);
     }
 
     public static void addBook(String bookName, int pageCount) {
-        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+        addBook(bookName, pageCount, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount, int priceBook) {
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр., " + (priceBook > 0 ? priceBook : "N/A") + " р.\n";
     }
 
     public static void printInfo() {
         System.out.println(booksInfo);
     }
 
+    public static int checkContinue() {
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Продолжить ввод? 1 - Да, 2 - Нет");
+            try {
+                int x = sc.nextInt();
+                if (x == 1 || x == 2) {
+                    return x;
+                } else {
+                    System.out.println("Вы ввели некорректную команду");
+                }
+            }catch (InputMismatchException x) {
+                System.out.println("Вы ввели не число. Можно ввести только 1 или 2");
+            }
+        }
+    }
 }
+
+
